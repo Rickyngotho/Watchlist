cc89f8a34b64bcc7b1b3ad680b6a1b9345a48c6b
Rickyngotho <gicheric14@gmail.com>
2018-11-08 19:12:45 +0300

    deployment to heroku

--- app/main/views.py
+++ app/main/views.py
@@ -1,9 +1,12 @@
 from flask import render_template,request,redirect,url_for
-from app import app
-from .request import get_movies,get_movie,search_movie
+from . import main
+from ..request import get_movies,get_movie,search_movie
+from .forms import ReviewForm
+from ..models import Review
+# Review = review.Review
 
 #Views
-@app.route('/movie/<int:id>')
+@main.route('/movie/<int:id>')
 def movie(id):
 
     '''
@@ -11,11 +14,12 @@ def movie(id):
     '''
     movie = get_movie(id)
     title = f'{movie.title}'
+    reviews = Review.get_reviews(movie.id)
 
-    return render_template('movie.html',title = title,movie = movie)
+    return render_template('movie.html',title = title,movie = movie,reviews = reviews)
 
 
-@app.route('/')
+@main.route('/')
 def index():
 
     '''
@@ -34,7 +38,7 @@ def index():
     else:
         return render_template('index.html',title = title,popular = popular_movies, upcoming = upcoming_movie,now_showing = now_showing_movie)
 
-@app.route('/search/<movie_name>')
+@main.route('/search/<movie_name>')
 def search(movie_name):
     '''
     View function to display the search results
@@ -44,3 +48,18 @@ def search(movie_name):
     searched_movies = search_movie(movie_name_format)
     title = f'search results for {movie_name}'
     return render_template('search.html',movies = searched_movies)
+
+@main.route('/movie/review/new/<int:id>', methods = ['GET','POST'])
+def new_review(id):
+    form = ReviewForm()
+    movie = get_movie(id)
+
+    if form.validate_on_submit():
+        title = form.title.data
+        review = form.review.data
+        new_review = Review(movie.id,title,movie.poster,review)
+        new_review.save_review()
+        return redirect(url_for('movie',id = movie.id ))
+
+    title = f'{movie.title} review'
+    return render_template('new_review.html',title = title, review_form=form, movie=movie)
---
+++ start.sh
@@ -0,0 +1,4 @@
+MOVIE_API_KEY = '9899088bafb9e6af4ea5ffa52af4f259'
+SECRET_KEY = '12345'
+
+python3.6 manage.py server
